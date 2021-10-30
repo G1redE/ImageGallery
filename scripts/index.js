d172ece01a8d79e76d9e69ac6f49559ba9ab0eda
G1redE <90474166+G1redE@users.noreply.github.com>
2021-10-30 16:16:25 +0530

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,313 @@
+const img_list =[
+    {
+        "url":"https://images.unsplash.com/photo-1634921553571-376a18dde21a?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMjF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"vintage camera"
+    
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635173007203-d9b90578525d?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Fruit Bowl"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635226881806-031cdc27296b?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Cute doggo"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635166076579-375d901bf53d?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"white house"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635219143988-e47f8c248d89?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"metro"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635225862023-ba555bde492b?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Tree in mountain"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635224626757-7de3747d9e99?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"mountain lake"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635215870586-fd905e595943?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"pumpkin"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635185113179-3fa7fbb4ca5e?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Tequilla"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635180723047-73e80a6fd02d?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"clown fish"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635172382208-f2642a8ddbbb?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"clouds"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635187498209-932fa157a4dc?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Legend Neon Sign"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635171616579-1c48c83fa01c?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"leaves"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635117743283-77fb124f8aae?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Trees green forest"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635194491684-8f71f85bdf40?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Woman hugging tree in snow"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635195890390-200a2535c351?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"water flow close shot"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635191002623-833161a6c105?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"lone wolf"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635179850458-333c83d91671?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"picnic"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635171660413-05f7eb476616?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"bag ,oranges and leafy vegetable"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635099066083-d61283752677?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"halloween pumpkin carving"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635107054269-d8a3163d4ae2?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"woman at lake"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1634993841974-e37dd3794593?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"mountain sunrise"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635205410406-d37ab6876111?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"shop with neon name board"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635108478357-ad6929589dbe?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"woman in field"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635105921323-b9e0b32f0a56?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzN3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"cool looking skater"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635012184362-46290da01599?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0Mnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"red flare"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635143110546-9504204233d0?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"cycle crossing road on zebra crossing"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635013864451-5ed0f271bbe0?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Nnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"people in metro during covid19 pandemic"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635099518034-0dbd09dce496?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"woman in bikini"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635107679809-2dcdea4b90e3?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"clouds around mountain"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1634710115840-245dc4ca7cef?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2Nnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"candles in dark"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1634913939327-97ce62f73f0e?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2N3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"workplace"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635164380064-d2d17fe5a2a3?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3MHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"flyover at night"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635092045771-bd4f553f78a6?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3MXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"waterfall"
+
+    },
+    {
+        "url":"https://images.unsplash.com/photo-1635144912648-febfca99f136?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4Mnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"go-karting high speed shot"
+
+    }
+    ,
+    {
+        "url":"https://images.unsplash.com/photo-1635408114234-47c57879df5b?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"owl"
+
+    }
+    ,
+    {
+        "url":"https://images.unsplash.com/photo-1635333638889-e7d59453ac8d?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "description":"Sun above clouds"
+    }
+]
+
+let n =Math.round((img_list.length)/3);
+
+console.log(img_list.length)
+let imgstr1="";
+let imgstr2="";
+let imgstr3="";
+let inner1 = document.getElementById('inner1');
+let inner2 = document.getElementById('inner2');
+let inner3 = document.getElementById('inner3');
+const cdiv = document.querySelector('.CenterDIV');
+const imagesdiv =document.querySelector('.ImagesDiv');
+const heading =document.querySelector('.header_heading');
+const zoomedimg = document.querySelector('.zoomedimg');
+
+for(let i=0;i<img_list.length;i++){
+    let imginnerdesc=document.createElement('div');
+    imginnerdesc.classList.add('imgdesc');
+    imginnerdesc.setAttribute('id',`imgdesc${i}`);
+    let imgcentrediv=document.createElement('div');
+    imgcentrediv.classList.add('imgcentrediv');
+    imgcentrediv.setAttribute('id','imgcentrediv');
+    let imgdownloadbtn=document.createElement('button');
+    imgdownloadbtn.classList.add('imgdownloadbtn');
+    imgdownloadbtn.setAttribute('id',`imgdownloadbtn${i}`);
+    imgdownloadbtn.innerHTML='<i class="fas fa-download"></i>';
+    let imgzoombtn =document.createElement('button');
+    imgzoombtn.classList.add('imgzoombtn');
+    imgzoombtn.setAttribute('id',`imgzoombtn${i}`);
+    imgzoombtn.innerHTML='<i class="fas fa-expand"></i>';
+   
+    if(i<n)
+    {   let imgdiv= document.createElement('div');
+        imgdiv.classList.add('imgdiv');
+        imgdiv.setAttribute('id',`imgdiv${i+1}`);
+        imgdiv.style.backgroundImage=`url(${img_list[i]["url"]})`;
+        imgdiv.innerHTML=`<img class="innerimg" title=${img_list[i]['description']} id="img${i+1}" src=${img_list[i]["url"]}/>`;
+        imginnerdesc.innerHTML+=`<p style='text-align:center;'>${img_list[i]['description']}</p>`;
+        imgdiv.appendChild(imginnerdesc); 
+        imgdiv.appendChild(imgcentrediv);
+        imgdiv.appendChild(imgdownloadbtn);
+        imgdiv.appendChild(imgzoombtn);
+        inner1.appendChild(imgdiv);
+    }
+    else if(i<2*n)
+    {    let imgdiv= document.createElement('div');
+        imgdiv.classList.add('imgdiv');
+        imgdiv.setAttribute('id',`imgdiv${i+1}`);
+        imgdiv.style.backgroundImage=`url(${img_list[i]["url"]})`;
+        imgdiv.innerHTML=`<img class="innerimg" title=${img_list[i]['description']} id="img${i+1}" src=${img_list[i]["url"]}/>`;
+        imginnerdesc.innerHTML+=`<p style='text-align:center;'>${img_list[i]['description']}</p>`;
+       
+        imgdiv.appendChild(imginnerdesc);
+        imgdiv.appendChild(imgcentrediv);
+        imgdiv.appendChild(imgdownloadbtn);
+        imgdiv.appendChild(imgzoombtn);
+
+        inner2.appendChild(imgdiv);
+    }
+    else{  
+        let imgdiv= document.createElement('div');
+        imgdiv.classList.add('imgdiv');
+        imgdiv.setAttribute('id',`imgdiv${i+1}`);
+        imgdiv.style.backgroundImage=`url(${img_list[i]["url"]})`;
+        imgdiv.innerHTML=`<img class="innerimg" title=${img_list[i]['description']} id="img${i+1}" src=${img_list[i]["url"]}/>`;
+        imginnerdesc.innerHTML+=`<p style='text-align:center;'>${img_list[i]['description']}</p>`;
+        imgdiv.appendChild(imginnerdesc);
+        imgdiv.appendChild(imgcentrediv);
+        imgdiv.appendChild(imgdownloadbtn);
+        imgdiv.appendChild(imgzoombtn);
+        inner3.appendChild(imgdiv);
+    }
+    imgcentrediv.addEventListener("pointerenter",()=>{
+        document.getElementById(`imgdesc${i}`).style.display="block";
+
+    })
+    imgcentrediv.addEventListener('pointerleave',()=>{
+        document.getElementById(`imgdesc${i}`).style.display="none";
+
+    })
+    imgzoombtn.addEventListener('click',()=>{
+        zoomedimg.setAttribute('src',img_list[i]["url"]);
+        opendiv();
+    });
+    imgdownloadbtn.addEventListener('click',()=>{
+        fetch(img_list[i]["url"])
+        .then((response) => response.blob())
+        .then((data) => {
+            const downloadurl = window.URL.createObjectURL(data);
+            const atag = document.createElement('a');
+            atag.href=downloadurl;
+            atag.download = 'download.jpg';
+            document.body.appendChild(atag);
+            atag.click();
+            atag.remove();
+        })
+
+    });
+
+    cdiv.addEventListener('click',()=>{
+        cdiv.classList.remove('show');
+        imagesdiv.style.display='flex';
+        heading.style.display='block';
+    });
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+function opendiv(){
+    cdiv.classList.add('show');
+    imagesdiv.style.display='none';
+    heading.style.display='none';
+   
+    
+}
+
